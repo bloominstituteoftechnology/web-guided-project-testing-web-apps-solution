e5613ba97a280cf3d7f520496ba5ba6614fbd0bb
Warren C. Longmire <warren.longmire@lambdaschool.com>
2020-12-03 10:14:49 -0500

    adds to lesson plan.

--- src/components/AnimalForm.test.js
+++ src/components/AnimalForm.test.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { render, fireEvent, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from '@testing-library/user-event';
 import AnimalForm from "./AnimalForm";
 
 test("renders App without errors", () => {
@@ -16,20 +17,18 @@ test("user can fill out and submit form", async () => {
   const notesInput = screen.getByLabelText(/notes/i);
 
   // fill out the form
-  fireEvent.change(speciesInput, { target: { value: "Grizzly Bear" } });
-  fireEvent.change(ageInput, { target: { value: 3 } });
-  fireEvent.change(notesInput, {
-    target: { value: "just a giant teddy bear" },
-  });
-
+  userEvent.type(speciesInput, "Grizzly Bear");
+  userEvent.type(ageInput, "3");
+  userEvent.type(ageInput, "just a giant teddy bear");
+  
   // assert that the form inputs have values (if you want)
   expect(speciesInput).toHaveValue("Grizzly Bear");
   // ... etc
 
   // Submit the form (Careful here... state changes can happen asynchronously)
   const button = screen.getByRole("button", { name: /submit!/i });
-  fireEvent.click(button);
+  userEvent.click(button);
 
   // assert that the animal has been added to the list
-  const newAnimal = await screen.findByText(/grizzly bear/i);
+  const newAnimal = screen.findByText(/grizzly bear/i);
 });
